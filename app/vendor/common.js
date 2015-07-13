ecabf481f6ae738b54a1325197cbd192c98acc89
AlexMil <milya89@gmail.com>
2015-07-14 01:41:35 +0300

    + fixed

--- app/vendor/common.js
+++ app/vendor/common.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 	$('#modalPoint').modal();
 	$('input, select').styler();
+	$('.button-del').click(function(){
+		$(this).parent('.button-obj').parent('.block-content').fadeOut('slow', function() {
+			$(this).parent('.button-obj').parent('.block-content').remove();
+		});
+		$(this).parent('.button-obj').parent('.block-listdoc').fadeOut('slow', function() {
+			$(this).parent('.button-obj').parent('.block-listdoc').remove();
+		});
+	});
 });
